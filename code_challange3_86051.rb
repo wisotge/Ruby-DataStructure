ffe7c081ae707e5749fbff758d4d8e68c879798b
wisotge <rnalstn0507@gmail.com>
2021-11-28 17:00:30 +0900

    code refactoring (불필요한 순회 횟수 제거)

--- code_challange3_86051.rb
+++ code_challange3_86051.rb
@@ -1,10 +1,7 @@
 NUMBERS = [1,2,3,4,6,7,8,0]
 
 def solution numbers
-  answer = 0
-  (1..9).to_a.map{|num| answer += num unless numbers.include?(num)}
-
-  return answer
+  return (1..9).to_a.sum - numbers.sum
 end
 
 solution(NUMBERS)
