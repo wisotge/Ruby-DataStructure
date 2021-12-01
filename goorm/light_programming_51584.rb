b89b7b1c8d2ec37d6ee8277324d996e9afcc0932
wisotge <rnalstn0507@gmail.com>
2021-12-02 00:32:23 +0900

    code refactoring

--- goorm/light_programming_51584.rb
+++ goorm/light_programming_51584.rb
@@ -1,4 +1,3 @@
-puts "값을 입력하여 주세요..."
 input = []
 
 while value = gets 
@@ -7,56 +6,51 @@ end
 
 def solution input
 	result = parse_input(input)
-	lamp_array = do_programming(result[:lamp_array], result[:p_array])
-	print_answer(lamp_array)
+	lamp_square = do_programming(result[:lamp_square], result[:p_array])
+	print_answer(lamp_square)
 end
 
 def parse_input input
-	# 전등 배열 크기 계산
 	size = input.shift.split(" ")	
-	height, width = size[0].to_i, size[1].to_i
+	column, row = size[0].to_i, size[1].to_i
 	
-	# 전등 배열 생성
-	lamp_array = []
-	height.times do
-		lamp_array << input.shift.split(" ")
-	end
+	lamp_square = []
+	column.times{lamp_square << input.shift.split(" ")}
 	
-	# 프로그래밍 하는 수 계산
 	p = input.shift.split(" ")[0].to_i
 	
-	# 프로그래밍 하는 줄 정보 생성
 	p_array = []
-	p.times do
-		p_array << input.shift.split(" ")
-	end
+	p.times{p_array << input.shift.split(" ") unless input.nil?}
 	
-	return {lamp_array: lamp_array, p_array: p_array}
+	return {lamp_square: lamp_square, p_array: p_array}
 end
 
-def do_programming lamp_array, p_array
+def make_opposite lamp
+	return lamp.eql?("0") ? "1" : "0"
+end
+
+def do_programming lamp_square, p_array
 	p_array.each do |p_info|
-		direction = p_info[0].eql?("0") ? "row" : "column"
-		target_i = p_info[1].to_i
+		direction = p_info[0].eql?("0") ? "vertical" : "horizontal"
+		target_i = p_info[1].to_i - 1
 		
-		if direction.eql?("row")
-			lamp_array[target_i - 1].each_with_index do |lamp, i|
-				lamp_array[target_i - 1][i] = (lamp == "0") ? "1" : "0"
-			end
-		elsif direction.eql?("column") 
-			lamp_array.each do |lamp|
-				lamp[target_i - 1] = (lamp[target_i - 1] == "0") ? "1" : "0"
-			end 
+		if direction.eql?("vertical")
+			lamp_line = lamp_square[target_i]
+			lamp_line.map.with_index{|lamp, i| lamp_line[i] = make_opposite(lamp)}
+		elsif direction.eql?("horizontal")
+			lamp_square.map{|lamp_line| lamp_line[target_i] = make_opposite(lamp_line[target_i])}
 		end 
+		
+		puts lamp_square.to_s
 	end
 	
-	return lamp_array
+	return lamp_square
 end 
 
-def print_answer lamp_array
+def print_answer lamp_square
 	result_msg = ""
-	lamp_array.each do |lamp|
-		lamp.map{|v| result_msg += "#{v} "}
+	lamp_square.each do |lamp_line|
+		lamp_line.map{|lamp| result_msg += "#{lamp} "}
 		result_msg += "\n"
 	end
 	
