bb60c7b4278765f1a2788a79130b7713de3ccc23
wisotge <rnalstn0507@gmail.com>
2021-12-10 23:17:05 +0900

    https://www.acmicpc.net/problem/11720 initial solve

---
+++ baekjoon/11720.rb
@@ -0,0 +1,9 @@
+# count = gets
+# numbers = gets
+
+# answer = 0
+# count.strip().to_i.times.with_index {|index| answer += numbers[index].to_i}
+
+# print answer
+
+gets;p gets.chars.map{|i|i.to_i}.sum
