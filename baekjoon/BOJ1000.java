2022f1c86b0b9dd5c525e0333e7143809437224c
wisotge <rnalstn0507@gmail.com>
2021-12-11 23:49:18 +0900

    https://www.acmicpc.net/problem/1000 initial solve

---
+++ baekjoon/BOJ1000.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class BOJ1000 {
+
+  public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+    int A = in.nextInt();
+    int B = in.nextInt();
+
+    System.out.println(A + B);
+
+    in.close();
+  }
+}
